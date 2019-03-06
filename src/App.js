760b64aa3b9945b28e4aeecebcd9c96980e7587b
Smilga <smilga@Smilgas-iMac.local>
2019-03-06 10:20:46 -0800

    PropTypes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import PropTypes from "prop-types";
+//install as seperate module prop-types
+//built in type checking to validate props
+//array,bool,func,number,object,string,symbol
+
+const Person = ({ img, name, age }) => {
+  return (
+    <article>
+      <img src={img} alt="person" />
+      <h4>name : {name}</h4>
+      <h4>age : {age}</h4>
+    </article>
+  );
+};
+Person.propTypes = {
+  img: PropTypes.string,
+  name: PropTypes.string,
+  age: PropTypes.number
+};
+
+class PersonList extends Component {
+  state = {
+    people: [
+      {
+        id: 1,
+        img: "https://randomuser.me/api/portraits/thumb/men/65.jpg",
+        name: "john",
+        age: 24
+      },
+      {
+        id: 2,
+        img: "https://randomuser.me/api/portraits/thumb/men/34.jpg",
+        name: "bob",
+        age: 27
+      }
+    ]
+  };
+  render() {
+    return (
+      <section>
+        {this.state.people.map(person => (
+          <Person
+            key={person.id}
+            img={person.img}
+            name={person.name}
+            age={person.age}
+          />
+        ))}
+      </section>
+    );
+  }
+}
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <PersonList />;
   }
 }
 
