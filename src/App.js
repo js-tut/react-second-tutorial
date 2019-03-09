d3950667edcccc2642ee2399e05b2385abc16c3b
Smilga <smilga@Smilgas-iMac.local>
2019-03-08 18:34:07 -0800

    React Fragment Functional this.setState()

--- src/App.js
+++ src/App.js
@@ -1,77 +1,59 @@
 import React, { Component } from "react";
 import "./App.css";
-//uncontrolled inputs
+//React Fragment and this.setState() is asynchronous
+//Functional this.setState()
 
-class Form extends Component {
+class Counter extends Component {
   state = {
-    firstName: "",
-    lastName: "",
-    people: []
+    count: 0
   };
-  handleChange = event => {
-    // console.log(event.target);
-    // console.log(event.target.name);
-    // console.log(event.target.value);
-
-    // if (event.target.name === "firstName") {
-    //   const textValue = event.target.value;
-
-    //   this.setState({
-    //     firstName: textValue
-    //   });
-    // }
-    const value = event.target.value;
-    this.setState({
-      [event.target.name]: value
-    });
+  hanleIncrease = () => {
+    // console.log("called first :", this.state.count);
+    this.setState(
+      {
+        count: this.state.count + 1
+      }
+      // () => console.log("called second :", this.state.count)
+    );
+    // this.setState({
+    //   count: this.state.count + 2
+    // });
+    // console.log("called third :", this.state.count);
   };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    const firstName = this.state.firstName;
-    const lastName = this.state.lastName;
-    if (firstName.length > 0 && lastName.length > 0) {
-      const person = ` ${firstName} ${lastName} `;
-      this.setState({
-        people: [...this.state.people, person],
-        firstName: "",
-        lastName: ""
-      });
-    }
+  hanleDecrease = () => {
+    console.log("called first :", this.state.count);
+    this.setState(
+      (state, props) => {
+        return { count: state.count - props.amount };
+      },
+      () => console.log("called second :", this.state.count)
+    );
+    // this.setState((state, props) => {
+    //   return { count: state.count - 2 };
+    // });
+    console.log("called third :", this.state.count);
   };
 
   render() {
     return (
-      <section>
-        <article>
-          <form onSubmit={this.handleSubmit}>
-            <input
-              type="text"
-              name="firstName"
-              value={this.state.firstName}
-              onChange={this.handleChange}
-            />
-            <input
-              type="text"
-              name="lastName"
-              value={this.state.lastName}
-              onChange={this.handleChange}
-            />
-            <button type="submit">submit</button>
-          </form>
-        </article>
-        <article>
-          <h1>people</h1>
-          <div>{this.state.people}</div>
-        </article>
-      </section>
+      <>
+        <div style={{ margin: "3rem", fontSize: "2rem" }}>
+          <button type="button" onClick={this.hanleDecrease}>
+            decrease
+          </button>
+          <span style={{ margin: "1rem" }}>count : {this.state.count}</span>
+          <button type="button" onClick={this.hanleIncrease}>
+            increase
+          </button>
+        </div>
+      </>
     );
   }
 }
 
 class App extends Component {
   render() {
-    return <Form />;
+    return <Counter amount="2" />;
   }
 }
 
