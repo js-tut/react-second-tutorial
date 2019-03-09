5ffa048badd549069b442dba8f4dfcc051f59fe7
Smilga <smilga@Smilgas-iMac.local>
2019-03-08 17:05:36 -0800

    Controlled Inputs Use Case

--- src/App.js
+++ src/App.js
@@ -3,27 +3,67 @@ import "./App.css";
 //uncontrolled inputs
 
 class Form extends Component {
+  state = {
+    firstName: "",
+    lastName: "",
+    people: []
+  };
+  handleChange = event => {
+    // console.log(event.target);
+    // console.log(event.target.name);
+    // console.log(event.target.value);
+
+    // if (event.target.name === "firstName") {
+    //   const textValue = event.target.value;
+
+    //   this.setState({
+    //     firstName: textValue
+    //   });
+    // }
+    const value = event.target.value;
+    this.setState({
+      [event.target.name]: value
+    });
+  };
+
   handleSubmit = e => {
     e.preventDefault();
-    const name = this.refs.myName;
-    const nameValue = name.value;
-
-    const email = this.email.value;
-
-    const text = this.refs.myText;
-    const textValue = text.textContent;
-    text.style.color = "red";
-    console.log(nameValue, email, textValue);
+    const firstName = this.state.firstName;
+    const lastName = this.state.lastName;
+    if (firstName.length > 0 && lastName.length > 0) {
+      const person = ` ${firstName} ${lastName} `;
+      this.setState({
+        people: [...this.state.people, person],
+        firstName: "",
+        lastName: ""
+      });
+    }
   };
+
   render() {
     return (
       <section>
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" ref="myName" />
-          <input type="email" ref={orange => (this.email = orange)} />
-          <button type="submit">submit</button>
-        </form>
-        <p ref="myText">hello world</p>
+        <article>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              name="firstName"
+              value={this.state.firstName}
+              onChange={this.handleChange}
+            />
+            <input
+              type="text"
+              name="lastName"
+              value={this.state.lastName}
+              onChange={this.handleChange}
+            />
+            <button type="submit">submit</button>
+          </form>
+        </article>
+        <article>
+          <h1>people</h1>
+          <div>{this.state.people}</div>
+        </article>
       </section>
     );
   }
